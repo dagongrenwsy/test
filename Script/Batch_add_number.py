9a295ac270b0c1a99529584c703f9c74f4275299
dagongrenwsy <1048323686@qq.com>
2023-10-24 20:14:35 +0800

    优化对表格序号处理脚本

--- Script/Batch_add_number.py
+++ Script/Batch_add_number.py
@@ -1,20 +1,22 @@
 import openpyxl
 import os
 
-s_min_row = (int)(input("请输入起始行数："))
-s_max_row = (int)(input("请输入结束行数："))
-s_min_col = (int)(input("请输入起始列数："))
-s_max_col = (int)(input("请输入结束列数："))
-file_path = input("请输入Excel文件路径：")
+file_path  = input("请输入Excel文件路径：")
+work_sheet = input("请输入处理的工作表：")
+s_min_row  = (int)(input("请输入起始行数："))
+s_max_row  = (int)(input("请输入结束行数："))
+s_min_col  = (int)(input("请输入起始列数："))
+s_max_col  = (int)(input("请输入结束列数："))
+
 directory = os.path.dirname(file_path)
 out_file_path = os.path.join(directory, 'modified_excel_file.xlsx')
-print(file_path)
+# print(file_path)
 
 # 打开Excel文件
 workbook = openpyxl.load_workbook(file_path)
 
 # 选择要处理的工作表
-sheet = workbook['TestSuite']  # 将 'Sheet1' 替换为您的工作表名称
+sheet = workbook[work_sheet]
 
 # 遍历要处理的单元格范围，假设范围是A1到A10
 for row in sheet.iter_rows(min_row=s_min_row, max_row=s_max_row, min_col=s_min_col, max_col=s_max_col):
@@ -31,7 +33,7 @@ for row in sheet.iter_rows(min_row=s_min_row, max_row=s_max_row, min_col=s_min_c
 
 # 保存修改后的Excel文件
 workbook.save(out_file_path)
-print(out_file_path)
+print("修改后的文件路径：" + out_file_path)
 
 # 关闭工作簿
 workbook.close()
